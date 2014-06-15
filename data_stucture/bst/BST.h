3504f32c0f008c16e966a71a0fad20383c5b92b0
xmandy <shenxm06@gmail.com>
2014-06-16 00:56:22 +0800

    Finished loop pre-in-postorder functions and loop_insert.

--- data_stucture/bst/BST.h
+++ data_stucture/bst/BST.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iomanip>
+#include <stack>
+
 using namespace std;
 
 #ifndef _BST_H
@@ -15,6 +17,16 @@ class BST
 
 		void inorder(ostream& out) const;
 
+		void preorder(ostream& out) const;
+
+		void postorder(ostream& out) const;
+
+		void inorder_loop(ostream& out) const;
+
+		void preorder_loop(ostream& out) const;
+
+		void postorder_loop(ostream& out) const;
+
 		void graph(ostream& out) const;
 
 		bool search(const T& item) const;
@@ -26,6 +38,8 @@ class BST
 		void insert(const T& item);
 
 		void remove(const T& item);
+
+		void remove_loop(const T& item);
 
 	private:
 
@@ -40,11 +54,24 @@ class BST
 				BinNode(T item): data(item), left(0), right(0) {};
 		};
 
+		class pair
+		{
+			public:
+				BinNode* node;
+				bool unUsed;
+
+				pair(BinNode* n, bool used): node(n), unUsed(used) {};
+		};
+
 		typedef BinNode * BinNodeP;
 
 		BinNodeP root;
 
 		void inorderAux(ostream& out, BinNodeP subtreeRoot) const;
+
+		void preAux(ostream& out, BinNodeP subtreeRoot) const;
+
+		void postAux(ostream& out, BinNodeP subtreeRoot) const;
 
 		void graphAux(ostream& out, int indent, BinNodeP subtreeRoot) const;
 
@@ -53,6 +80,8 @@ class BST
 		void insertAux(const T& item, BinNodeP& subtreeRoot);
 
 		void removeAux(const T& item, BinNodeP &subtreeRoot);
+
+		void searchWithParent(T& item, bool& found, BinNodeP& x, BinNodeP& parent);
 
 		
 	
@@ -69,11 +98,96 @@ void BST<T>::inorderAux(ostream& out, BST<T>::BinNodeP subtreeRoot) const
 	}
 }
 
+template <typename T>
+void BST<T>::preAux(ostream& out, BinNodeP subtreeRoot) const
+{
+	if(subtreeRoot != NULL)
+	{
+		out << subtreeRoot->data << " ";
+		preAux(out, subtreeRoot->left);
+		preAux(out, subtreeRoot->right);
+	}
+}
+
+template <typename T>
+void BST<T>::postAux(ostream& out, BinNodeP subtreeRoot) const
+{
+	if(subtreeRoot != NULL)
+	{
+		postAux(out, subtreeRoot->left);
+		postAux(out, subtreeRoot->right);
+		out << subtreeRoot->data << " ";
+	}
+}
+
 
 template <typename T>
 inline void BST<T>::remove(const T& item)
 {
 	removeAux(item, root);
+}
+
+template <typename T>
+void BST<T>::searchWithParent(T& item, bool& found, BST<T>::BinNodeP& x, BST<T>::BinNodeP& parent)
+{
+	x = root;
+	parent = NULL;
+	found = false;
+	while(!found && x)
+	{
+		if(item < x->data)
+		{
+			parent = x;
+			x = x->left;
+		}
+		else if(item > x->data)
+		{
+			parent = x;
+			x = x->right;
+		}
+		else
+		{
+			found = true;
+		}
+	}
+}
+
+template <typename T>
+inline void BST<T>::remove_loop(const T& item)
+{
+	bool found;
+	BST<T>::BinNodeP x, parent;
+	searchWithParent(item, found, x, parent);
+
+	if(!found)
+		cout << "item to be deleted is not in the tree" <<endl;
+
+	if(x->left && x->right)
+	{
+		BST<T>::BinNodeP xSuc = x->right;  // find the success node in middle order,
+		while(xSuc->left)
+		{
+			parent = xSuc;
+			xSuc = xSuc->left;
+		}
+		x->data = xSuc->data;
+		x = xSuc;
+	}
+	//after the find success node, the x node must be a node has no child or only has
+	//right child.
+
+	// the node has only one not null subtree node.
+
+	BST<T>::BinNodeP subtree = x->left;
+	if(subtree == NULL)
+		subtree = x->right;
+	if(root == NULL)
+		root = subtree;
+	else if(parent->left == x)
+		parent->left = subtree;
+	else
+		parent->right = subtree;
+	delete x;
 }
 
 template <typename T>
@@ -100,6 +214,98 @@ inline void BST<T>::inorder(ostream& out) const
 {
 	inorderAux(out, root);
 	out << endl;
+}
+
+template <typename T>
+inline void BST<T>::preorder(ostream& out) const
+{
+	preAux(out, root);
+	out << endl;
+}
+
+template <typename T>
+inline void BST<T>::postorder(ostream& out) const
+{
+	postAux(out, root);
+	out << endl;
+}
+
+template <typename T>
+void BST<T>::inorder_loop(ostream& out) const
+{
+	stack<BST<T>::pair> s;
+	s.push(pair(root, true));
+	pair *p;
+	BST<T>::BinNodeP temp;
+	int unUsed;
+	while(!s.empty())
+	{
+		p = &s.top();
+		s.pop();
+		temp = p->node;
+		unUsed = p->unUsed;
+
+		if(unUsed)
+		{
+			if(temp->right)
+				s.push(pair(temp->right, true));
+			s.push(pair(temp, false));
+			if(temp->left)
+				s.push(pair(temp->left, true));
+		}
+		else
+			cout << temp->data << " ";
+
+	}
+	cout << endl;
+
+}
+
+template <typename T>
+void BST<T>::preorder_loop(ostream& out) const
+{
+	stack<BST<T>::BinNodeP> s;
+	BST<T>::BinNodeP temp;
+	s.push(root);
+	while(!s.empty())
+	{
+		temp = s.top();
+		s.pop();
+		cout << temp->data << " ";
+		if(temp->right)
+			s.push(temp->right);
+		if(temp->left)
+			s.push(temp->left);
+	}
+	cout << endl;
+}
+
+template <typename T>
+void BST<T>::postorder_loop(ostream& out) const
+{
+	stack<BST<T>::pair> s;
+	pair *p;
+	BST<T>::BinNodeP temp;
+	int unUsed;
+	s.push(pair(root, true));
+	while(!s.empty())
+	{
+		p = &s.top();
+		s.pop();
+		temp = p->node;
+		unUsed = p->unUsed;
+		if(unUsed)
+		{
+			s.push(pair(temp, false));
+			if(temp->right)
+				s.push(pair(temp->right, true));
+			if(temp->left)
+				s.push(pair(temp->left, true));
+		}
+		else
+			cout << temp->data << " ";
+	}
+	cout << endl;
 }
 
 template <typename T>
--- data_stucture/bst/main.cpp
+++ data_stucture/bst/main.cpp
@@ -16,22 +16,28 @@ int main(int argc, const char *argv[])
 	cout << "Inorder"<<endl;
 	intBST.inorder(cout);
 
-	int number;
-	for(;;)
-	{
-		cout << "input the number: ";
-		cin >> number;
-		if(number < 0)
-			break;
-		intBST.insert(number);
-	}
+	//int number;
+   /* for(;;)*/
+	//{
+		//cout << "input the number: ";
+		//cin >> number;
+		//if(number < 0)
+			//break;
+		//intBST.insert(number);
+	/*}*/
+	intBST.insert(50);
+	intBST.insert(30);
+	intBST.insert(70);
+	intBST.insert(20);
+	intBST.insert(40);
+	intBST.insert(60);
+	intBST.insert(80);
 
 	intBST.graph(cout);
 
 	intBST.search(9);
 	intBST.search(100);
 
-	intBST.inorder(cout);
 
    /* intBST.remove(100);*/
 	//intBST.remove(12);
@@ -42,6 +48,13 @@ int main(int argc, const char *argv[])
 	intBST.remove(6);
 	intBST.graph(cout);
 
+	intBST.preorder(cout);
+	intBST.inorder(cout);
+	intBST.postorder(cout);
+
+	intBST.preorder_loop(cout);
+	intBST.inorder_loop(cout);
+	intBST.postorder_loop(cout);
 
 	return 0;
 }
